796f583c741c49bb35bb4a9c53d1b4d001a4c68b
iVlady <vinogrotskiy97@gmail.com>
2021-05-23 19:47:14 +0300

    Made functionality

--- Color Changer/ViewController.swift
+++ Color Changer/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayColorLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var redTextLabel: UILabel!
+    @IBOutlet weak var redSliderValue: UILabel!
+    
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var greenTextSlider: UILabel!
+    @IBOutlet weak var greenSliderValue: UILabel!
+    
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var blueTextLabel: UILabel!
+    @IBOutlet weak var blueSliderValue: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    func colorTheLabel () {
+        displayColorLabel.backgroundColor = UIColor (red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    @IBAction func rgbSlider() {
+        colorTheLabel()
+    }
+    
 
 }
 
